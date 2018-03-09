587dd89708d0a41407194748fae5ffc067787869
Austin Smith <smithaustin0129@gmail.com>
2018-03-08 22:40:07 -0500

    Formatting change to GNU style

--- State.h
+++ State.h
@@ -5,10 +5,10 @@
 
 class State
 {
-	public:
-		virtual void handle_input(SDL_Event &e) = 0;
-		virtual void update_logic() = 0;
-		virtual void draw(SDL_Renderer *renderer) = 0;
-		virtual bool is_running() = 0;
+ public:
+  virtual void handle_input(SDL_Event &e) = 0;
+  virtual void update_logic() = 0;
+  virtual void draw(SDL_Renderer *renderer) = 0;
+  virtual bool is_running() = 0;
 };
 #endif
